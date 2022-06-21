d41ee2e5d9d1c87186b59d052fff0b08e8fe0388
adofsauron <adofsauron@163.com>
2022-06-21 17:26:38 +0800

    fix main

--- hashtable/src/hashtable.cpp
+++ hashtable/src/hashtable.cpp
@@ -5,87 +5,8 @@
 #include "hashtable.h"
 #include "bucket.h"
 
-int ExtensibleHashTable::hash(int key) {
-    int index;
-    index = ((key+484)*5533) % directory_size;
-    return index;
-}
+//public 
 
-void ExtensibleHashTable::split(int key) {
-    int index;
-    index = this->hash(key);
-    Bucket* newBucket = new Bucket(bucket_size);
-    newBucket->local_depth = bucketArr[index]->local_depth; // copy the local depth
-
-    if (global_depth == bucketArr[index]->local_depth) {
-        this->doubleArr();
-        int newIndex = this->hash(key);
-        for (int i = 0; i < bucket_size; i++) {
-            if (hash(bucketArr[index]->valueArr[i]) == newIndex) {
-                newBucket->Insert(bucketArr[index]->valueArr[i]);
-                bucketArr[index]->Remove(bucketArr[index]->valueArr[i]);
-            }
-        }
-
-        newBucket->local_depth++;
-        bucketArr[newIndex] = newBucket;
-        //add the depth number with same connection
-        int index_same_connection;
-        if (newIndex >= directory_size/2) {
-            index_same_connection = newIndex - int(pow(2,global_depth-1)); 
-        } else {
-            index_same_connection = newIndex + int(pow(2,global_depth-1)); 
-        } 
-
-        bucketArr[index_same_connection]->local_depth++;
-    } else {
-        int newIndex;
-        if (index >= directory_size/2) {
-            newIndex = index - int(pow(2,global_depth-1)); 
-        } else {
-            newIndex = index + int(pow(2,global_depth-1)); 
-        } 
-        for (int i = 0; i < bucket_size; i++) {
-            if (hash(bucketArr[index]->valueArr[i]) == newIndex) {
-                newBucket->Insert(bucketArr[index]->valueArr[i]);
-                bucketArr[index]->Remove(bucketArr[index]->valueArr[i]);
-            }   
-        }
-        newBucket->local_depth++;
-        bucketArr[newIndex] = newBucket;
-        bucketArr[index]->local_depth++;
-    }
-}
-
-void ExtensibleHashTable::doubleArr() {
-    Bucket **newArr = new Bucket* [2*(directory_size)];
-
-    for (int i = 0; i < directory_size; i++) {
-        int newIndex = int(pow(2,global_depth));
-        newArr[i] = bucketArr[i];
-        newArr[i+newIndex] = bucketArr[i];
-    }
-    bucketArr = newArr;
-
-    global_depth++;
-    directory_size = int(pow(2,global_depth));
-}
-
-int ExtensibleHashTable::getSize() {
-    return bucket_size;
-}
-
-int ExtensibleHashTable::getDS() {
-    return directory_size;
-}
-
-
-int ExtensibleHashTable::getGD() {
-    return global_depth;
-}
-
-
-//public function:
 ExtensibleHashTable::ExtensibleHashTable() {
     this->global_depth = 1;
     this->bucket_size = 4;
@@ -164,3 +85,83 @@ void ExtensibleHashTable::Print() {
     }
 }
 
+// private 
+
+int ExtensibleHashTable::hash(int key) {
+    int index;
+    index = ((key+484)*5533) % directory_size;
+    return index;
+}
+
+void ExtensibleHashTable::split(int key) {
+    int index;
+    index = this->hash(key);
+    Bucket* newBucket = new Bucket(bucket_size);
+    newBucket->local_depth = bucketArr[index]->local_depth; // copy the local depth
+
+    if (global_depth == bucketArr[index]->local_depth) {
+        this->doubleArr();
+        int newIndex = this->hash(key);
+        for (int i = 0; i < bucket_size; i++) {
+            if (hash(bucketArr[index]->valueArr[i]) == newIndex) {
+                newBucket->Insert(bucketArr[index]->valueArr[i]);
+                bucketArr[index]->Remove(bucketArr[index]->valueArr[i]);
+            }
+        }
+
+        newBucket->local_depth++;
+        bucketArr[newIndex] = newBucket;
+        //add the depth number with same connection
+        int index_same_connection;
+        if (newIndex >= directory_size/2) {
+            index_same_connection = newIndex - int(pow(2,global_depth-1)); 
+        } else {
+            index_same_connection = newIndex + int(pow(2,global_depth-1)); 
+        } 
+
+        bucketArr[index_same_connection]->local_depth++;
+    } else {
+        int newIndex;
+        if (index >= directory_size/2) {
+            newIndex = index - int(pow(2,global_depth-1)); 
+        } else {
+            newIndex = index + int(pow(2,global_depth-1)); 
+        } 
+        for (int i = 0; i < bucket_size; i++) {
+            if (hash(bucketArr[index]->valueArr[i]) == newIndex) {
+                newBucket->Insert(bucketArr[index]->valueArr[i]);
+                bucketArr[index]->Remove(bucketArr[index]->valueArr[i]);
+            }   
+        }
+        newBucket->local_depth++;
+        bucketArr[newIndex] = newBucket;
+        bucketArr[index]->local_depth++;
+    }
+}
+
+void ExtensibleHashTable::doubleArr() {
+    Bucket **newArr = new Bucket* [2*(directory_size)];
+
+    for (int i = 0; i < directory_size; i++) {
+        int newIndex = int(pow(2,global_depth));
+        newArr[i] = bucketArr[i];
+        newArr[i+newIndex] = bucketArr[i];
+    }
+    bucketArr = newArr;
+
+    global_depth++;
+    directory_size = int(pow(2,global_depth));
+}
+
+int ExtensibleHashTable::getSize() {
+    return bucket_size;
+}
+
+int ExtensibleHashTable::getDS() {
+    return directory_size;
+}
+
+
+int ExtensibleHashTable::getGD() {
+    return global_depth;
+}
